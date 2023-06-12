a7b58144f9f03766d0a8c423c1e6d9b1d2a023cb
Meekl-e <nogovitsin111@gmail.com>
2023-06-12 15:21:22 +0700

    fix

---
+++ MachineLearning/analyzeRabbitML/main.py
@@ -0,0 +1,13 @@
+from CreatingDataSet import GeneratingRabbits
+from MachineLearningAI import neuralNetwork
+
+gen = GeneratingRabbits(100)
+gen.generatingRabbits()
+gen.generatingNoRabbits()
+
+model = neuralNetwork()
+
+
+
+
+
